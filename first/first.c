5f50b125e23fe6fe9498d0c2e1480acc0e0a1e0a
Oliver Gilan <olivergilan@gmail.com>
2018-09-17 15:22:33 -0400

    fgetc getting wrong integer

--- first/first.c
+++ first/first.c
@@ -15,12 +15,36 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    //Gets file from arguments
-    input = argv[1];
-    //Gets item count from file and creates array
-    char array[fgetc(input)];
+    //Opens file
+    input = fopen(argv[1], "r");
+    if(input == NULL){
+        printf("Could not open file!");
+        return 1;
+    }else{
+        printf("File Opened \n");
+    }
 
+    //Gets item count from file and creates array
+    rewind(input);
+    int count = fgetc(input);
+    printf("COUNT: %d \n", count);
+    int array[count];
     
-    
+    //Reads each character and adds it as integer to array
+    for(int i = 0; i<count;i++){
+        int c = (int) fgetc(input);
+        if(feof(input)){
+            break;
+        }else{
+            array[i] = c;
+        }
+    }
+
+    //Closes file
+    fclose(input);
+
+    for(int i=0;i<argc;i++){
+        printf("#%d is %s \n", i, argv[i]);
+    }
     return 0;
 }
